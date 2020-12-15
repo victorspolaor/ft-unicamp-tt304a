a26e6102baaa7eb88487c83216b50e5ec8b9cc17
TiagoOM17 <tiagoomendes15@gmail.com>
2020-12-15 18:09:56 -0300

    Mudanças após a consulta com o professor

--- Projeto2.c
+++ Projeto2.c
@@ -8,31 +8,27 @@
 
 #define N 20                         //Numero definido para a quantidade de post-its que o pombo suporta
 
+
 sem_t mutex, vazio, cheio, pombosem; //Definindo semaforos
 int cont_postit;                     //Definindo variavel globais que contará os post-its
 time_t t;                            //Definindo variavel de tempo para utilizar o srand com mais variedade
 
 //Chamadas de funçoes
-void reseta();
-void fcolapostit();
-void leva_mochila_ate_B_e_volta();
 void *usuarios(void*);
 void *pombof(void*);
 
 //Funçao Main
 int main(){
 
-    int i, x;                                                 //Definindo variaveis auxiliares
+    int i, x, *id;                                                 //Definindo variaveis auxiliares
     cont_postit = 0;                                          //Definindo valor inicial para a quantidade de post-its
     printf("Quantos usuarios escreveram nos post-its?\nR: "); //Perguntando ao usuario quantas threads o programa ira utilizar
     scanf("%i", &i); 
     pthread_t usuario[i], pombo;                              //Definindo as threads
 
     //iniciando os semaforos
-    sem_init(&mutex, 1, 1);
-    sem_init(&pombosem, 1, 0);
-    sem_init(&vazio, 0, N);
-    sem_init(&cheio, 0, 0);
+    sem_init(&mutex, 0, 1);
+    sem_init(&pombosem, 0, 0);
 
     //inicializando o pombo;
     pthread_create(&pombo, NULL, pombof, NULL);
@@ -40,7 +36,9 @@ int main(){
 
     //inicializando os usuarios
     for (x = 0; x < i; x++){
-        pthread_create(&(usuario[x]), NULL, usuarios, NULL);
+        id = (int *)malloc(sizeof(int));
+        *id = x + 1;
+        pthread_create(&(usuario[x]), NULL, usuarios, id);
     }
 
     pthread_join(pombo, NULL);
@@ -49,64 +47,55 @@ int main(){
     }
 }
 
-
-//Funçao que reseta os semaforos que controlam o "buffer" de post- its
-void reseta(){
-    for(int i = 0;i < 20; i++){
-        sem_post(&vazio);
-        sem_wait(&cheio);
-    }
-}
-
 //Funçao para colar o postit
-void fcolapostit(){
-    sleep(1 + rand() % 1); //Espera um tempo para o usuario colar o post-it 
-    cont_postit++;         //e logo em segui incrementa um bilhete no contador
-    printf("Algum usuario colou um postit. Total de postit: %i\n", cont_postit);
-    if(cont_postit == 20){ //Verifica se existem 20 post-it no pombo para que ele posso envia-los
-        sem_post(&pombosem);
-    }
+void colapostit(void *arg){
+    int id = *((int *) arg);
+
+    sleep(1 + rand() % 1);   //Espera um tempo para o usuario colar o post-it 
+    printf("O usuario %d colou um postit. Total de postit: %i\n", id, cont_postit);
+    
 }
 
-//Funçao que define o momento que o pombo vai ate o lado b e o tempo q ele vai levar pra volta
-void leva_mochila_ate_B_e_volta()
-{
-    cont_postit = 0; //Quando o pombo sai para levar os bilhetes eles sao zerados
+//Funçao que define quanto tempo o pombo demorará na viagem
+void leva_mochila_ate_B_e_volta(){
     srand(time(NULL));
-    sleep(1 + (rand() % 1));
-    reseta();
-    printf("O pombo retornou e irá dormir enquanto espera os 20 post-it\n");
-    sem_wait(&pombosem);
-    sem_post(&mutex);
-
+    sleep(1 + (rand() % 3));
 
 }
 
-
+//Funçao que define o tempo q o usuario demorará pra escrever o post-it
+void dormealeatorio(){
+    srand(time(NULL));
+    sleep(1 + (rand() % 3));
+}
 
 //Funçao para o funcionamento das Threads Usuarios
-void *usuarios(void *p_arg){
+void *usuarios(void *arg){
     while (1){
-        if(cont_postit == 20){
+        sem_wait(&mutex);
+        dormealeatorio();
+        colapostit(arg);
+        cont_postit++; 
+        if(cont_postit == N){
+            printf("ATENÇAO! A MOCHILA ESTA CHEIA!!!\n");
             sem_post(&pombosem);
             sem_wait(&mutex);
+            
         }
-        sem_wait(&mutex);
-        sem_wait(&vazio);
-        
-        fcolapostit();
-        
         sem_post(&mutex);
-        sem_post(&cheio);
     }
 }
 
 //Funçao para o funcionamento da Thread Pombo
 void *pombof(void *p_arg){
-    while (1){
+    while (1){        
         sem_wait(&pombosem);
-        printf("O pombo acordou e ira levar as mensagens ate o ponto B\n");
+        printf("**O pombo acordou e ira levar as mensagens ate o ponto B**\n");
         leva_mochila_ate_B_e_volta();
-        
+        printf("**O pombo retornou e irá dormir enquanto espera os 20 post-it**\n");
+        cont_postit = 0;
+        sem_post(&mutex);
+        sem_wait(&pombosem);
     }
 }
+
